c60e590ef5bde32ea089c684922f7cb6ef82323c
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-04-08 10:54:37 -0600

    10:54AM

---
+++ Chapter8/src/Associate.java
@@ -0,0 +1,38 @@
+
+public class Associate extends Employee
+{
+	private double hourlyPayRate;
+	
+	public Associate(String fn, String ln, double r)
+	{
+		super(fn, ln);
+		hourlyPayRate = r;
+	}
+	
+	public double pay(double hours)
+	{
+		double payEarned;
+		
+		if(hours > 40)
+		{
+			payEarned = ((hourlyPayRate *40)
+						+ (hourlyPayRate * 1.5)*(hours - 40));
+		}
+		else
+		{
+			payEarned = hours * hourlyPayRate;
+		}
+		return payEarned;
+	}
+	
+	public String toString()
+	{
+		return(super.toString()
+				+ ", Associate"
+				+ "Pay Rate: "
+				+ hourlyPayRate);
+	}
+	
+	
+
+}
