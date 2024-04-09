67a63a99b7ab11d340b2efbb5988314d6ffd3bf8
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-04-09 09:23:02 -0600

    9:22AM

---
+++ Chapter8/src/CresMart/Employee.java
@@ -0,0 +1,22 @@
+package CresMart;
+
+public abstract class Employee 
+{
+	private String firstName, lastName;
+	
+	//Constructor method for Employee 
+	public Employee(String fN, String lN)
+	{
+		firstName = fN;
+		lastName = lN;
+	}
+	
+	public String toString()
+	{
+		return(firstName + " "+ lastName);
+	}
+	
+	abstract double pay(double period);
+	
+	
+}
---
+++ Chapter8/src/CresMart/Manager.java
@@ -0,0 +1,40 @@
+package CresMart;
+
+public class Manager extends Employee 
+{
+	private double yearlySalary;
+	
+	//A manager object has been created
+	public Manager(String fN, String lN, double S)
+	{
+		super(fN, lN);
+		yearlySalary = S;
+	}
+	
+	//Return the Manager salary
+	public double getYearlySalary()
+	{
+		return yearlySalary;
+	}
+	
+	//Return the manager pay for a specified period
+	public double pay(double weeks)
+	{
+		double payEarned;
+		
+		payEarned = yearlySalary / 52 * weeks;
+		
+		return payEarned;
+	}
+	
+	//Returns the employee name, title, and salary
+	public String toString()
+	{
+		return(super.toString()
+				+ ", Manager"
+				+ "Salary: "
+				+ yearlySalary);
+	}
+	
+
+}
