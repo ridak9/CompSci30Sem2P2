45ddb264111cfc4e5c4a0c3ba9d4625c29d6f652
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-04-11 09:36:22 -0600

    9:37AM

--- Chapter8/src/University/Staff.java
+++ Chapter8/src/University/Staff.java
@@ -1,5 +1,30 @@
 package University;
 
-public class Staff {
-
+public class Staff extends UEmployee
+{
+	private String title;
+	
+	public Staff(String fn, String ln, double s, String t)
+	{
+		super(fn, ln, s);
+		title = t;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	public void setTitle(String t)
+	{
+		title = t;
+	}
+	
+	public String toString()
+	{
+		return(super.getFirstName()
+				+ super.getLastName()
+				+ super.getSalary()
+				+ title);
+	}
+	
 }
