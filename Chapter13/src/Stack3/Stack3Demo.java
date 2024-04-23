f7ff4fe77d62486e3d5d1f311c2a51a06765b27a
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-04-23 09:17:03 -0600

    9:17AM

--- Chapter13/src/Stack3/Stack3Demo.java
+++ Chapter13/src/Stack3/Stack3Demo.java
@@ -1,8 +1,10 @@
 package Stack3;
 
-public class Stack3Demo {
+public class Stack3Demo 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
