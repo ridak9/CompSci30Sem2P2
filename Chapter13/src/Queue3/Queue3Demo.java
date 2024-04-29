1d5d3add06414cb8ba6534422f2c2623db4a499f
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-04-29 11:10:31 -0600

    11:10AM

--- Chapter13/src/Queue3/Queue3Demo.java
+++ Chapter13/src/Queue3/Queue3Demo.java
@@ -5,6 +5,32 @@ public class Queue3Demo
 
 	public static void main(String[] args) 
 	{
+		Queue3 q = new Queue3();
+
+		System.out.println("Adding \"red\", \"yellow\", and \"orange\" to queue.");
+		q.enqueue("red");
+		q.enqueue("yellow");
+		q.enqueue("orange");
+		System.out.println("Front of queue: " + (String)q.front());
+		System.out.println("Items in queue: " + q.size());
+		System.out.println("Dequeueing front item: " + (String)q.dequeue());
+		System.out.println("Front of queue: " + (String)q.front());
+		System.out.println("Items in queue: " + q.size());
+		System.out.println("Dequeueing front item: " + (String)q.dequeue());
+		System.out.println("Front of queue: " + (String)q.front());
+		System.out.println("Items in queue: " + q.size());
+		System.out.println("Adding \"purple\" to queue.");
+		q.enqueue("purple");
+		System.out.println("Front of queue: " + (String)q.front());
+		System.out.println("Items in queue: " + q.size());
+		System.out.println("Dequeueing front item: " + (String)q.dequeue());
+		System.out.println("Adding \"lime\" to queue.");
+		q.enqueue("lime");
+		System.out.println("Front of queue: " + (String)q.front());
+		System.out.println("Items in queue: " + q.size());
+		System.out.println("Dequeueing front item: " + (String)q.dequeue());
+		System.out.println("Dequeueing front item: " + (String)q.dequeue());
+		System.out.println("Items in queue: " + q.size());
 		
 
 	}
